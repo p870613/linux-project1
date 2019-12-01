45632601bdee30ba8a1d89c1fff287dccf152eab
fff321755 <105201032@cc.ncu.edu.tw>
2019-12-01 16:27:43 +0800

    final version(v15)

---
+++ simple_sys.c
@@ -0,0 +1,172 @@
+//請先看過https://blog.csdn.net/Lzq_CNQF/article/details/83148013
+//請加在 yourkernel/kernel/sys.c 的 #endif /* CONFIG_COMPAT */ 之前
+//版本 4.17.4
+
+//function for convert virtual address to physical address
+static unsigned long convert_to_phy_add(struct mm_struct *mm, unsigned long long vir_add)
+{
+	unsigned long phy_add;
+	pgd_t *pgd = NULL;
+	pud_t *pud = NULL;
+	pmd_t *pmd = NULL;
+	pte_t *pte = NULL;
+	p4d_t *p4d = NULL;
+	struct page *pg = NULL;
+
+	pgd = pgd_offset(mm, vir_add);
+
+	if(pgd_none(*pgd) || pgd_bad(*pgd))
+		return 0;
+
+	p4d = p4d_offset(pgd, vir_add);
+
+	if(p4d_none(*p4d) || p4d_bad(*p4d))
+		return 0;
+
+	pud = pud_offset(p4d, vir_add);
+
+	if(pud_none(*pud) || pud_bad(*pud))
+		return 0;
+
+	pmd = pmd_offset(pud, vir_add);
+
+	if(pmd_none(*pmd) || pmd_bad(*pmd))
+		return 0;
+	
+	if (!(pte = pte_offset_map(pmd, vir_add)))
+        return 0;
+
+	if (!(pg = pte_page(*pte)))
+		return 0;
+
+	phy_add = page_to_phys(pg);
+	pte_unmap(pte);
+
+
+	return phy_add;
+
+}
+
+// save and printk the projects problems
+void vm_table(struct task_struct *task, char* result)
+{
+	//counter number of pages with no physical address / number of pages / number of VMA
+	int no_phy_count = 0, tatol_count = 0, vma_count = 0;
+    
+	unsigned long vpage;
+	struct vm_area_struct *vma =  0;
+
+	//for store data
+	int i;
+	int title_offset = 11;
+	char* title_1 = "PAGE_TABLE\n";
+	char* title_2 = "COUNTERS\n";
+	for(i=0;i<11;i++)
+		result[i] = title_1[i];
+	char str[100];
+	int ret;
+	int write_count = title_offset;
+
+	for(vma = task->mm->mmap ;vma; vma = vma -> vm_next){	
+		vma_count++;
+		//printk("VMA: %d\n",vma_count);
+		
+		for (vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE){
+
+			tatol_count++;
+
+			//if no physical adress, it will print and save as 0x000000000 to 0x000000000
+			if(convert_to_phy_add(task->mm, vpage) == 0){
+				no_phy_count++;
+				
+				
+				printk(	"[VMA%d] [0x%012lx,0x%012lx] | [0x%09lx,0x%09lx] \n",
+					vma_count ,
+					vpage ,vpage + PAGE_SIZE,
+					convert_to_phy_add(task->mm, vpage),convert_to_phy_add(task->mm, vpage)
+					);
+
+				//save virtual/physical address intervel and which VMA the page in to result[]
+				ret = snprintf(str, 80,"%d\t0x%012lx\t0x%012lx\t0x%09lx\t0x%09lx\n",
+								vma_count,
+								vpage,vpage+PAGE_SIZE,
+								convert_to_phy_add(task->mm, vpage),convert_to_phy_add(task->mm, vpage));
+
+				for(i=0;i<ret;i++)
+				{
+					result[write_count] = str[i];
+					write_count++;
+				}
+
+			}
+			else{
+				
+				printk(	"[VMA%d] [0x%012lx,0x%012lx] | [0x%09lx,0x%09lx] \n",
+						vma_count ,
+						vpage ,vpage + PAGE_SIZE,
+						convert_to_phy_add(task->mm, vpage),convert_to_phy_add(task->mm, vpage) + PAGE_SIZE
+						);
+
+				//save virtual/physical address intervel and which VMA the page in to result[]
+				ret = snprintf(str, 80,"%d\t0x%012lx\t0x%012lx\t0x%09lx\t0x%09lx\n",
+									vma_count,
+									vpage,vpage+PAGE_SIZE,
+									convert_to_phy_add(task->mm, vpage),convert_to_phy_add(task->mm, vpage)+PAGE_SIZE);
+						
+				for(i=0;i<ret;i++)
+				{
+					result[write_count] = str[i];
+					write_count++;
+				}
+			}
+
+		}
+	}
+
+
+	// save the number of pages and number of pages with no physical address
+	for(i=0;i<9;i++)
+	{
+		result[write_count] = title_2[i];
+		write_count++;
+	}
+
+
+	ret = snprintf(str, 10,"%d\n", no_phy_count);
+
+	for(i=0;i<ret;i++)
+	{
+		result[write_count] = str[i];
+		write_count++;
+	}
+
+	ret = snprintf(str, 10,"%d\n", tatol_count);
+
+	for(i=0;i<ret;i++)
+	{
+		result[write_count] = str[i];
+		write_count++;
+	}
+
+	printk("no_phy_count:%d\n",no_phy_count);
+	printk("total_count:%d\n",tatol_count);
+
+}
+
+
+
+
+//syscall 334 linux_survey_TT
+
+
+SYSCALL_DEFINE1(linux_survey_TT,char*,result){
+
+  	struct task_struct *task = get_current();
+	printk("[v15] YahaRo\n");
+	printk("--------Table v15--------\n");
+    vm_table(task,result);
+	printk("[v15] ByeBye~\n");
+
+    return;
+
+}
--- test.c
+++ test.c
@@ -5,44 +5,54 @@
 #include <wait.h>//wait function
 #include <string.h>
 
-#define  MEMORY_SIZE  100000
+#define  MEMORY_SIZE  100000	//not 1000000 since it's too big that trigs segmentation fault
+#define  linux_survey_TT(result) syscall(334,result)  // in syscall_64.tbl the linux_survey_TT is defined to syscall NO.334
 
 
 int main()
 {  
-	int result_1[MEMORY_SIZE];
+	int result_1[MEMORY_SIZE];	//acording to the project website, we use int array not char array
 	int result_2[MEMORY_SIZE];
 	int result_3[MEMORY_SIZE];
+
 	int exit_status;
 
 
 	if(fork())
 	{   //executed by the parent process
 
-		syscall(334,result_1);      //location 1
+
+		linux_survey_TT(result_1);      //location 1
 		wait(&exit_status);
 
+		//Save result_1 to txt
 		FILE *fpWrite1=fopen("result_1.txt","w");
 		fprintf(fpWrite1,"%s",result_1);
 		fclose(fpWrite1); 
+
+
 	} 
 	else
 	{   //executed by the child process
 		
 
-		syscall(334,result_2);        //location 2
+		linux_survey_TT(result_2);     //location 2
 
 		result_1[0]=123;               //This should trigger copy-on-right, if the above system call does not trigger the COW.
 
-		syscall(334,result_3);         //location 3
+		linux_survey_TT(result_3);     //location 3
 
+		//Save result_2 to txt
 		FILE *fpWrite2=fopen("result_2.txt","w");
 		fprintf(fpWrite2,"%s",result_2);
 		fclose(fpWrite2); 
-	
+		
+		//Save result_3 to txt
 		FILE *fpWrite3=fopen("result_3.txt","w");
 		fprintf(fpWrite3,"%s",result_3);
 		fclose(fpWrite3); 
-	}	
+
+	}
+	
 	return 0;
 }  
